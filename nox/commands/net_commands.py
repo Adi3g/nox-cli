a37decea3e307dab420bfe1cd764603c72801708
Adib Grouz <capsule_spat_0j@icloud.com>
2024-08-25 09:12:32 +0200

    add traceroute, whois ... cmds to netManager

--- nox/commands/net_commands.py
+++ nox/commands/net_commands.py
@@ -43,7 +43,111 @@ def scan(host: str, ports: str) -> None:
     click.echo(result)
 
 
+@click.command()
+@click.option(
+    '--host', required=True,
+    help='The host to perform traceroute on',
+)
+@click.option('--max-hops', default=30, help='Maximum number of hops')
+def traceroute(host: str, max_hops: int) -> None:
+    """Perform a traceroute to a specified host."""
+    result = NetworkManager.traceroute(host, max_hops)
+    click.echo(result)
+
+
+@click.command()
+@click.option(
+    '--domain', required=True,
+    help='The domain to perform WHOIS lookup on',
+)
+def whois(domain: str) -> None:
+    """Perform a WHOIS lookup to get information about a domain."""
+    result = NetworkManager.whois_lookup(domain)
+    click.echo(result)
+
+
+@click.command()
+@click.option(
+    '--ip', required=True,
+    help='The IP address to get geolocation info for',
+)
+def geoip(ip: str) -> None:
+    """Get geolocation information for an IP address."""
+    result = NetworkManager.geoip_lookup(ip)
+    click.echo(result)
+
+
+@click.command()
+@click.option(
+    '--url', required=True,
+    help='The URL to perform an HTTP GET request on',
+)
+def http(url: str) -> None:
+    """Perform an HTTP GET request to a URL
+    and display the response status and headers."""
+    result = NetworkManager.http_request(url)
+    click.echo(result)
+
+
+@click.command()
+def speedtest() -> None:
+    """Test the download and upload bandwidth to/from a server."""
+    result = NetworkManager.speedtest()
+    click.echo(result)
+
+
+@click.command()
+@click.option(
+    '--subnet', required=True,
+    help='The subnet to perform a ping sweep on (e.g., 192.168.1.0/24)',
+)
+def ping_sweep(subnet: str) -> None:
+    """Perform a ping sweep over a range of IP addresses
+    to see which hosts are active."""
+    result = NetworkManager.ping_sweep(subnet)
+    click.echo(result)
+
+
+@click.command()
+@click.option(
+    '--host', required=True,
+    help='The host to check TCP connection',
+)
+@click.option(
+    '--port', required=True,
+    type=int, help='The port to check TCP connection',
+)
+def tcp_check(host: str, port: int) -> None:
+    """Check if a TCP connection to a specified port on a host is possible."""
+    result = NetworkManager.tcp_check(host, port)
+    click.echo(result)
+
+
+@click.command()
+def arp() -> None:
+    """Display the ARP (Address Resolution Protocol)
+    cache on the local machine."""
+    result = NetworkManager.arp_cache()
+    click.echo(result)
+
+
+@click.command()
+def ifconfig() -> None:
+    """Display information about network interfaces on the local machine."""
+    result = NetworkManager.ifconfig()
+    click.echo(result)
+
+
 # Add commands to the group
 net.add_command(ping)
 net.add_command(dns)
 net.add_command(scan)
+net.add_command(traceroute)
+net.add_command(whois)
+net.add_command(geoip)
+net.add_command(http)
+net.add_command(speedtest)
+net.add_command(ping_sweep)
+net.add_command(tcp_check)
+net.add_command(arp)
+net.add_command(ifconfig)
