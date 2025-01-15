5a0f04b0336b75d991f8a5babbc61f318b3125b7
Adib Grouz <capsule_spat_0j@icloud.com>
2025-01-15 19:11:43 +0000

    hash tests

--- tests/hash_commands_test.py
+++ tests/hash_commands_test.py
@@ -35,6 +35,6 @@ def test_verify_hash(hash_manager, tmp_path):
             test_file,
         ), '3de8f8b0dc94b8c2230fab9ec0ba0506', 'md5',
     ), 'Hash verification failed.'
-    assert not hash_manager.verify_hash(
+    assert not hash_manager.verify_hash_from_file(
         str(test_file), 'wronghashvalue', 'md5',
     ), 'Hash verification should fail.'
